4abb301ff00e76359fa24c49e593acc659149f58
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-07 23:36:13 +0000

    Check - Hold the Door challenge - Level 0

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """ Write a script or a program that votes 1024 times for your id here: http://158.69.76.135/level0.php. """
+import requests
 
+url = "http://158.69.76.135/level0.php"
+
+with requests.session() as client:
+	data = {"id": 1992, "holdthedoor": "submit"}
+	for cont in range(1024):
+		client.post(url, data)
