2d27f581e7cbd81197d8bacd58f156673ccd46c6
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-17 20:54:08 +0000

    Fix in level 3

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -3,9 +3,9 @@
 """ We only take votes from Windows users. """
 import requests
 try:
-    import Image
-except ImportError:
     from PIL import Image
+except ImportError:
+    import Image
 import pytesseract
 
 url = "http://158.69.76.135/level3.php"
@@ -14,16 +14,18 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 
 
 with requests.session() as client:
     for i in range(10):
-        key = client.get(url)
-        img_captcha = client.get(captcha, cookies=key.cookies)
+        key = client.get(url, headers=headers)
+        img_captcha = client.get(captcha, headers=headers)
         key_dic = key.cookies.get_dict()
         cookies = key.cookies['HoldTheDoor']
-        phpsessid = key.cookies['PHPSESSID']
-        with open("captcha_img.png", 'wb') as f:
-            for j in img_captcha.iter_content():
-                f.write(j)
-        open_img = Image.open("captcha_img.png")
-        open_img.load()
-        convert_text = pytesseract.image_to_string(open_img)
-        data_post = {"id": 19920001, "holdthedoor": "Submit", "key": key_dic['HoldTheDoor'], "captcha": convert_text}
-        client.post(url, data=data_post, cookies={'HoldTheDoor': cookies, 'PHPSESSID': phpsessid}, headers=headers)
+        # phpsessid = key.cookies['PHPSESSID']
+
+        open_img = open('captcha_img.png', 'wb')
+        open_img.write(img_captcha.content)
+        open_img.close()
+
+        convert_text = pytesseract.image_to_string(Image.open('captcha_img.png'))
+        print(convert_text, type(convert_text))
+
+        data_post = {"id": 93012, "holdthedoor": "Submit", "key": key_dic['HoldTheDoor'], "captcha": convert_text}
+        client.post(url, data=data_post, cookies={'HoldTheDoor': cookies}, headers=headers)
